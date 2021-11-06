44bfeba0759ea7a210a594a49f5faab98dd7e50a
bmkuter <60325789+bmkuter@users.noreply.github.com>
2021-11-06 10:58:22 -0400

    Updating for code review

--- Firmware/Smart_Grow/pwm_functions.h
+++ Firmware/Smart_Grow/pwm_functions.h
@@ -9,14 +9,17 @@
 	#include "WProgram.h"
 #endif
 
+
+
 struct PWM_Pump {
 	uint8_t pin;
 	uint8_t min;
 	uint8_t max;
 };
 
-void PWM_Calibration(PWM_Pump pwm_pump);
-void PWM_set_percent(PWM_Pump pwm_pump, uint8_t percent);
+void PWM_Calibration(PWM_Pump *pwm_pump);
+void PWM_set_percent(PWM_Pump *pwm_pump, uint8_t percent);
+void Dose_food(PWM_Pump* pwm_pump, uint8_t ml);
 
 #endif
 
