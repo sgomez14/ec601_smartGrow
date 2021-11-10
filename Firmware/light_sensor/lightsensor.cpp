59701e7f6b918d399708318b6e5690960abb25bc
Yuan Sui <90477124+Smile-NEKO@users.noreply.github.com>
2021-11-09 21:54:17 -0500

    Add files via upload

--- Firmware/light_sensor/lightsensor.cpp
+++ Firmware/light_sensor/lightsensor.cpp
@@ -1,7 +1,10 @@
 #include "lightsensor.h"
 
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
-
+uint32_t lum = 0;
+uint16_t ir = 0;
+uint16_t full = 0;
+uint16_t lux=0;
 void light_sensor_init_info(void)
 {
   /* Display the gain and integration time for reference*/  
@@ -45,24 +48,45 @@ void init_light_sensor(void){
   TSL2591_INTEGRATIONTIME_500MS
   TSL2591_INTEGRATIONTIME_600MS  // longest integration time (dim light)
 */
-
-  tsl.setGain(TSL2591_GAIN_MED); 
-
-  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
-
+    if (tsl.begin()) 
+  {
+    Serial.println(F("Found a TSL2591 sensor"));
+    tsl.setGain(TSL2591_GAIN_MED); 
+    tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  } 
+  else 
+  {
+    Serial.println(F("No sensor found ... check your wiring?"));
+    while (1);
+  }
 }
 
-
+//Print light data
 void print_light_data(void)
 {
-  // Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
-  uint32_t lum = tsl.getFullLuminosity();
-  uint16_t ir, full;
+  lum = tsl.getFullLuminosity();
   ir = lum >> 16;
   full = lum & 0xFFFF;
+  lux=tsl.calculateLux(full, ir);
+  // print 32 bits with top 16 bits IR, bottom 16 bits full spectrum
   Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
   Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
   Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
   Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
-  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
+  Serial.print(F("Lux: ")); Serial.println(lux, 6);
+}
+//Check that the light data is correct or not
+bool read_light_data(void){
+   // Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+  lum = tsl.getFullLuminosity();
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  lux=tsl.calculateLux(full, ir);
+  int test_value=ir*full*lux;
+  if (test_value==0){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
